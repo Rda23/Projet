c071fd0ed6709f154cf34df0afa7417a4146a522
Darren ASSALE <darrenassale@leschassagnes.net>
2024-07-15 14:28:32 +0200

    nouveau

--- nouveau.php
+++ nouveau.php
@@ -15,12 +15,12 @@
 
    <div class="box">
       <form action='verif.php' method="POST" class="case" >
-         Nom: <input type="text" name="nom"/>
-         prix: <input type="text" name="prix"/>
-         quantité: <input type="text" name="quantite"/>
-         date d'ajout: <input type="text" name="datedajout"/>
-         date de modification: <input type="text" name="datedemodif"/>
-         photo: <input type="text" name="photo"/>
+         Nom : <input type="text" name="nom"/>
+         prix : <input type="text" name="prix"/>
+         quantité : <input type="text" name="quantite"/>
+         date d'ajout : <input type="text" name="datedajout"/>
+         date de modification : <input type="text" name="datedemodif"/>
+         photo : <input type="text" name="photo"/>
          <br><br>
          <input class="envoyer" type="submit" value="Enregistrer">
       </form>
